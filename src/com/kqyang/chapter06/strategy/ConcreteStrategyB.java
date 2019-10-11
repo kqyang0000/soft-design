7ed0db4d91464ecdcec9f1d1dd654886a7b1cdf7
kqyang <kqyang0000@163.com>
2019-10-11 23:29:46 +0800

    策略模式的使用

---
+++ src/com/kqyang/chapter06/strategy/ConcreteStrategyB.java
@@ -0,0 +1,8 @@
+package com.kqyang.chapter06.strategy;
+
+public class ConcreteStrategyB implements Strategy {
+    @Override
+    public void strategyMethod() {
+        System.out.println("具体策略B的策略方法被访问");
+    }
+}
